da0f4385351c8863159c12a911cc2f5471f3ea1a
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-07-29 17:34:06 +0545

    make function for login

--- client/src/pages/login/login-form.tsx
+++ client/src/pages/login/login-form.tsx
@@ -2,7 +2,9 @@ import { useForm } from 'react-hook-form'
 
 import FormContainer from '@/components/form-container'
 import Input from '@/components/input'
+import AuthService from '@/services/auth.service'
 import { LoginProps } from '@/types/forms'
+import { useMutation } from '@tanstack/react-query'
 
 const LoginForm = () => {
   const {
@@ -11,7 +13,18 @@ const LoginForm = () => {
     formState: { errors },
   } = useForm<LoginProps>()
 
+  const { mutate } = useMutation({
+    mutationFn: AuthService.login,
+    onError: () => {
+      console.log('Error!')
+    },
+    onSuccess: () => {
+      console.log('Success!')
+    },
+  })
+
   const onSubmit = (data: LoginProps) => {
+    mutate(data.password)
     console.log(data)
   }
 
@@ -23,12 +36,15 @@ const LoginForm = () => {
       </div>
       <form className="flex flex-col gap-y-1" onSubmit={handleSubmit(onSubmit)}>
         <Input<LoginProps>
+          type="password"
           error={errors.password}
           name="password"
           register={register}
           placeholder="Password"
         />
-        <button> Submit! </button>
+        <button className="self-center rounded-lg bg-blue-400 px-10 py-2 duration-300 hover:bg-blue-500">
+          Submit
+        </button>
       </form>
     </FormContainer>
   )
