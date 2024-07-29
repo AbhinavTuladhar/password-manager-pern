31981af3ec6b4787681c99714cb169fe58d220c3
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-07-29 11:28:22 +0545

    configure buttons

--- client/src/components/button/index.tsx
+++ client/src/components/button/index.tsx
@@ -9,13 +9,13 @@ const Button: FC<ButtonProps> = ({ variant, children, ...props }) => {
   return (
     <button
       className={classNames(
-        'relative z-10 overflow-hidden rounded-lg border border-black px-4 py-3 after:absolute after:inset-0 after:-z-10 after:origin-right after:scale-x-0 after:rounded-lg after:transition-transform after:duration-300 hover:after:origin-left hover:after:scale-x-100',
+        'relative z-10 overflow-hidden rounded-lg border px-4 py-3 after:absolute after:inset-0 after:-z-10 after:origin-right after:scale-x-0 after:transition-transform after:duration-300 hover:after:origin-left hover:after:scale-x-100',
         {
-          'border-white after:bg-blue-700 hover:border-blue-700 hover:text-white':
+          'border-white bg-blue-700 after:bg-blue-400 hover:border-blue-700 hover:text-black':
             variant === 'transparent',
         },
         {
-          'border-orange-500 bg-orange-500 after:bg-blue-500 hover:border-white':
+          'border-blue-300 bg-blue-300 text-black after:bg-blue-700 hover:border-blue-400 hover:text-white':
             variant === 'coloured',
         },
         {},
--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -4,7 +4,7 @@ import Button from '../button'
 
 const Navbar = () => {
   return (
-    <header className="flex w-full items-center justify-between bg-blue-600 px-4 py-5">
+    <header className="flex w-full items-center justify-between bg-gradient-to-r from-blue-800 via-indigo-600 to-blue-700 p-4">
       <h1 className="text-2xl font-bold">PassManager</h1>
       <nav className="flex gap-x-4">
         <Link to="/register">
