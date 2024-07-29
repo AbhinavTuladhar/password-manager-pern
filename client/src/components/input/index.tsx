2add13526a74e4e283db0fd8ecfd83d6de469003
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-07-29 14:43:34 +0545

    uninstall zod, add error message

--- client/src/components/input/index.tsx
+++ client/src/components/input/index.tsx
@@ -1,25 +1,30 @@
 import { FC, InputHTMLAttributes } from 'react'
-import type { UseFormRegister } from 'react-hook-form'
+import type { FieldError, RegisterOptions, UseFormRegister } from 'react-hook-form'
 
 import { RegisterFields, RegisterProps } from '../../types/forms'
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string
   name: RegisterFields
   register: UseFormRegister<RegisterProps>
+  error: FieldError | undefined
+  options?: RegisterOptions<RegisterProps, RegisterFields>
 }
 
-const Input: FC<InputFieldProps> = ({ label, name, register, ...props }) => {
+const Input: FC<InputFieldProps> = ({ options, name, register, error, ...props }) => {
   return (
-    <div className="relative">
-      <input
-        {...props}
-        {...register(name)}
-        className="peer w-full border-b border-b-white bg-transparent pb-2 focus:outline-none"
-      />
-      <label className="absolute left-0 top-0 text-slate-400 duration-300 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-white">
-        {label}
-      </label>
+    <div className="flex flex-col">
+      <div className="relative">
+        <input
+          {...props}
+          {...register(name, options)}
+          className="peer w-full border-b border-b-white bg-transparent pb-2 focus:outline-none"
+        />
+      </div>
+      {error ? (
+        <p className="text-sm text-red-500">{error.message}</p>
+      ) : (
+        <p className="invisible text-sm"> Error message</p>
+      )}
     </div>
   )
 }
--- client/src/components/register-form/index.tsx
+++ client/src/components/register-form/index.tsx
@@ -1,5 +1,8 @@
 import { useForm } from 'react-hook-form'
 
+import { useMutation } from '@tanstack/react-query'
+
+import AuthService from '../../services/auth.service'
 import { RegisterProps } from '../../types/forms'
 import Input from '../input'
 
@@ -7,11 +10,23 @@ const RegisterForm = () => {
   const {
     register,
     handleSubmit,
-    // formState: { errors },
+    formState: { errors },
+    setError,
     // reset,
   } = useForm<RegisterProps>()
 
+  const { mutate } = useMutation({
+    mutationFn: AuthService.register,
+  })
+
   const onSubmit = (data: RegisterProps) => {
+    mutate(data.password)
+    if (data.password !== data.confirmPassword) {
+      setError('confirmPassword', {
+        message: 'Passwords do not match',
+      })
+      return
+    }
     console.log(data)
   }
 
@@ -19,9 +34,33 @@ const RegisterForm = () => {
     <div className="w-container space-y-12 rounded-md bg-indigo-700 p-8 shadow-md shadow-slate-600">
       <h2 className="text-xl font-bold"> Registration form</h2>
       <form className="flex flex-col gap-y-8" onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-14">
-          <Input name="password" register={register} label="Password" />
-          <Input name="confirmPassword" register={register} label="Confirm password" />
+        <div className="space-y-6">
+          <Input
+            minLength={1}
+            error={errors.password}
+            name="password"
+            register={register}
+            placeholder="Password"
+            options={{
+              required: {
+                value: true,
+                message: 'Please enter your password.',
+              },
+            }}
+          />
+          <Input
+            minLength={1}
+            error={errors.confirmPassword}
+            name="confirmPassword"
+            register={register}
+            placeholder="Confirm password"
+            options={{
+              required: {
+                value: true,
+                message: 'Please enter your password confirmation.',
+              },
+            }}
+          />
         </div>
         <button className="self-center rounded-lg bg-blue-400 px-10 py-2 duration-300 hover:bg-blue-500">
           Submit
