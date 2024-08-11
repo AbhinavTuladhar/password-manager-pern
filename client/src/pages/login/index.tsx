8dd3bd32628a8519c95a88cdced4f10f8b78a014
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-08-11 14:33:49 +0545

    feat: add container class to main part of UI

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -2,7 +2,7 @@ import LoginForm from './login-form'
 
 const Login = () => {
   return (
-    <div className="grid flex-1 place-items-center">
+    <div className="grid h-full flex-1 place-items-center">
       <LoginForm />
     </div>
   )
