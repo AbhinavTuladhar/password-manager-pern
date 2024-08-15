81d19960d0fed3c94b2b0c970e7838d97e47bdd1
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-08-15 21:54:54 +0545

    feat: add GET request for accounts

--- server/controllers/account.controller.ts
+++ server/controllers/account.controller.ts
@@ -1,7 +1,40 @@
 import { Request, Response } from 'express'
 import prisma from '../prisma'
 import { AccountBody, WebsiteBody } from '../types'
-import { encrypt } from '../utils/crypto.utils'
+import { decrypt, encrypt } from '../utils/crypto.utils'
+
+export const getAllAccounts = async (req: Request, res: Response) => {
+  const accounts = await prisma.account.findMany({
+    include: {
+      website: {
+        select: {
+          name: true,
+          url: true,
+        },
+      },
+    },
+  })
+
+  // Send only the relevant data in the api response.
+  const reducedData = accounts.map((account) => {
+    const {
+      password,
+      userName,
+      website: { name: websiteName, url: websiteUrl },
+    } = account
+
+    const decryptedPassword = decrypt(password)
+
+    return {
+      userName,
+      password: decryptedPassword,
+      websiteName,
+      websiteUrl,
+    }
+  })
+
+  return res.status(200).json({ message: 'Success!', data: reducedData })
+}
 
 export const addAccount = async (
   req: Request<{ websiteId: string }, {}, AccountBody>,
--- server/routes/account.route.ts
+++ server/routes/account.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { addAccount } from '../controllers/account.controller'
+import { addAccount, getAllAccounts } from '../controllers/account.controller'
 
 const accountRouter = Router()
 
+accountRouter.get('/', getAllAccounts)
 accountRouter.post('/', addAccount)
 
 export default accountRouter
